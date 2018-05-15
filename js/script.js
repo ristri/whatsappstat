125b7964d99f0593af2399cedf33cc9e333aff61
Rishabh Tripathi <ristripathi97@gmail.com>
2018-05-15 14:11:51 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -33,28 +33,32 @@ window.onload = function () {
                     var time = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
                     var emoji = fileReader.result.match(/[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug)
                     sender.forEach(function (element, i) {
-                        sender[i] = sender[i].replace("- ", "").replace(":", "").replace(" ", "").replace("]","");
+                        sender[i] = sender[i].replace("- ", "").replace(":", "").replace("]", "");
                     });
                     sender.forEach(element => {
                         count[element] = (count[element] || 0) + 1;
                     });
-                    if (emoji!==null) {
+                    if (emoji !== null) {
                         emoji.forEach(element => {
                             countEmoji[element] = (countEmoji[element] || 0) + 1;
                         });
                     }
                     datetime.forEach(element => {
-                        date.push(element.match(/(\d+[\/.]\d+[\/.]\d+)/g)[0]);
-                        if (element.search(/[aA]/) != -1)
-                            time[Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", "")) % 12]++;
-                        else if (element.search(/[pP]/) != -1)
-                            time[Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", "")) == 12 ? 12 : Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", "")) + 12]++;
-                        else
-                            time[Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", ""))]++;
-                    });
+                        try {
+                            date.push(element.match(/(\d+[\/.]\d+[\/.]\d+)/g)[0]);
+                            if (element.search(/[aA]/) != -1)
+                                time[Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", "")) % 12]++;
+
+                            else if (element.search(/[pP]/) != -1)
+                                time[Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", "")) == 12 ? 12 : Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", "")) + 12]++;
+                            else
+                                time[Number(element.match(/[-+]?\s\d+:/g)[0].replace(":", "").replace(" ", ""))]++;
+                        }
+                        catch(TypeError){};
+                        });
                     plotSenderGraph(count);
                     plotTimeGraph(time);
-                    if (emoji!==null)
+                    if (emoji !== null)
                         plotEmojiGraph(countEmoji);
                     plotDayGraph(date);
                     initial.style.display = "none";
